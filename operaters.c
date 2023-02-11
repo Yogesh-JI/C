a0a718f6d6b91baa1967950ad4f46011f319ca01
YOGESH <yogeshverma02349@gmail.com>
2023-02-11 21:03:41 +0530

    just it

---
+++ operaters.c
@@ -0,0 +1,11 @@
+//wap to calculate the speed of a car
+#include <stdio.h>
+#include <conio.h>
+int main()
+
+{
+float speed ,distance=50,time=12;
+speed=distance/time;
+printf("speed of the car=%f",speed);
+
+}
