d667c6ab43858e9b97498e29c41fdb8310325d92
YOGESH <yogeshverma02349@gmail.com>
2023-02-12 21:47:14 +0530

    just it

---
+++ specifier.c
@@ -0,0 +1,21 @@
+//WAP to print the details of a stodent like name,registration number,roll number,city,state,contanct number,CGPA
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+char name[10]="yogesh",city[100]="singrauli", state[10]="MP";
+
+double reg_number=12214796,contact_number=64565465465,roll_number=1;
+float cgpa=4.2;
+
+printf("\n my name=%s",name);
+printf("\n my reg. number=%lf",reg_number);
+printf("\n roll number=%lf",roll_number);
+printf("\n contact number=%lf",contact_number);
+printf("\n my cgpa=%lf",cgpa);
+printf("\n my city=%s",city);
+printf("\n state=%s",state);
+//printf("");
+//printf("");
+
+}
