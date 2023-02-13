bdf558bbcf6d6323f89d7cedc387ca4eddc0c272
YOGESH <yogeshverma02349@gmail.com>
2023-02-13 18:55:56 +0530

    just it

---
+++ second class.c
@@ -0,0 +1,15 @@
+//WAP to print the details of a stodent like name,registration number,roll number,city,state,contanct number,CGPA
+#include <stdio.h>
+#include <conio.h>
+int main()
+
+{
+printf("\n name=yogesh ji verma");
+printf("\n registratio number= 234523214");
+printf("roll number=01");
+printf("city= singrauli");
+printf("contact number=363647458345");
+printf("CGPA=4.00");
+
+
+}
