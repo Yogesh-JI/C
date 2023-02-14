39a7df9863189df0d601ab3f540bb7b2a244af53
YOGESH <yogeshverma02349@gmail.com>
2023-02-14 21:10:55 +0530

    just it

---
+++ bitwise operator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+
+{
+	int a,b;
+	printf("enter the valuse of a: ");
+	scanf("%d",&a);
+	printf("enter the value of b: ");
+	scanf("%d",&b);
+	printf("a=%d,b=%d\n",a,b);
+	printf("a&b=%d\n",a&b);
+	printf("a|b=%d\n",a|b);
+	printf("a^b=%d\n",a^b);
+	printf("~a=%d\n",~a);
+	printf("b<<1=%d\n",b<<1);
+	printf("b>>1=%d\n",b>>1);
+		
+}
