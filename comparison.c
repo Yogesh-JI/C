f35aeda43ce3772d1871e15716887997ed74bea7
YOGESH <yogeshverma02349@gmail.com>
2023-02-15 21:09:32 +0530

    just it

---
+++ comparison.c
@@ -0,0 +1,16 @@
+//wap to find whether the number entered by user is smallest or greatest out of 2 variables
+#include <stdio.h>
+int main()
+
+{
+	int a,b;
+	printf("enter the first value: ");
+	scanf("\%d",&a);
+	printf("enter the second value: ");
+	scanf("%d",&b);
+	if (a>b)
+		printf("a is greatest");
+    else
+		printf("b is greatest");
+		
+}
