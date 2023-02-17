e1d245e15cba1a263deaa4050d984cc6af48d904
YOGESH <yogeshverma02349@gmail.com>
2023-02-17 21:12:05 +0530

    just it

---
+++ user input.c
@@ -0,0 +1,16 @@
+//wap for user input
+#include <stdio.h>
+#include <conio.h>
+int main()
+
+{
+	int rollno;
+	char name;
+	printf("enter your rollno: ");
+	scanf("%d",&rollno);
+	printf("enter your name: ");
+	scanf("%s",&name);
+	printf("\n hello my name is %s",&name);
+	printf("\n hello my rollno is %d",&rollno);
+	return 0;
+}
