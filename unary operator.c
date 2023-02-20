2166e7d44be63b20cabee04df84f646ed063f888
YOGESH <yogeshverma02349@gmail.com>
2023-02-20 21:05:42 +0530

    just it

---
+++ unary operator.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+//{
+//
+//int x= 3,y,z;
+//y=x++;
+//z=++x;
+//printf("\n%d,%d,%d",x,y,z);
+//
+//}
+
+
+//{
+//	int x,y,z;
+//	printf("enter x= ");
+//	scanf("%d",&x);
+//	y=x++;
+//    z=++x;
+//    printf("\n%d,%d,%d",x,y,z);
+//}
+
+//{
+//	int a=1,b=1,c;
+//	c=a++ +b;
+//	printf("\n%d,%d,%d",a,b,c);
+//	
+//}
+
+{
+	int x,y,z;
+	
+	printf("enter x= ");
+	scanf("%d",&x);
+	y=--x;
+	z=x--;
+	printf("\n%d,%d,%d",x,y,z);
+}
