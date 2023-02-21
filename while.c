14d05f5a0677207d167df3caa482a0f0f40761d8
YOGESH <yogeshverma02349@gmail.com>
2023-02-21 21:27:46 +0530

    just it

---
+++ while.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+
+{
+	int n;
+	printf("enter the number: ");
+	scanf("%d",&n);
+	while(n>0)
+	{
+	 printf("\n%d",n);
+	
+	 n=n-1;
+    }
+}    
+
