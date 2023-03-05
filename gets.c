9a97dd61bacf71a6cd56add5fa0242d435b5ac41
YOGESH <yogeshverma02349@gmail.com>
2023-03-05 23:57:18 +0530

    just it

---
+++ gets.c
@@ -0,0 +1,30 @@
+//#inlcude <stdio.h>
+//#include <conio.h>
+
+main()
+//{
+//	char ch[30];
+//	printf("enter the string: ");
+//	gets(ch);
+//	puts("entered string:");
+//	puts(ch);
+//}
+
+//wap to ask the user to enter name,city and state and then display the user using unformatted fn
+
+{
+	char n[30];char c[30];char s[30];
+	puts("enter the name: ");
+	gets(n);
+	puts("enter the city: ");
+	gets(c);
+	puts("enter the state: ");
+	gets(s);
+	puts("the entered details are:");
+//	puts("name:");
+	puts(n);
+//	puts("city:");
+	puts(c);
+//	puts("state:");
+	puts(s);
+}
