020ba9b66ecee2d8cea558510532af9eec2d3a86
YOGESH <yogeshverma02349@gmail.com>
2023-03-16 22:44:32 +0530

    module of library

---
+++ math library.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int x,y;
+    printf("enter the value for the x:");
+    scanf("%d",&x);
+    printf("enter the value for the y:");
+    scanf("%d",&y);
+
+    printf("the square root of the x is: %.2f",sqrt(x));
+    printf("the exponential of the x is: %.2f",exp(x));
+    printf("the natura; logerithm of the x is: %.2f",sqrt(x));
+    printf("the logeret of the x is: %.2f",sqrt(x));
+
+    
+}
+
+// wap  to perform +,-,*,/ using user define fn
+// wap to ask the user to enter the details and display the details using user defines fn
