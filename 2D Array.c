1de340607ef24c033de5f80da1c5091cd4063a40
Yogesh-JI <yogeshverma02349@gmai.com>
2023-03-29 23:57:32 +0530

    types of array

---
+++ 2D Array.c
@@ -0,0 +1,34 @@
+//wap to declare ,initialize and display the values of 2D array
+#include<stdio.h>
+int main()
+{
+int i,j,r,c;
+int a[2][2]={{1,2},{3,4}};
+for (i=0;i<2;i++)
+{
+    for(j=0;j<2;j++)
+    {
+        printf("%d",a[i][j]);
+    }
+    printf("\n");
+}
+
+
+// int n[r][c];
+
+// for (i=0;i<2;i++)
+// {
+//     for(j=0;j<2;j++)
+//     {
+//         printf("%d",a[i][j]);
+//     }
+//     printf("\n");
+// }
+
+
+
+}
+
+
+
+
