c90e74f709749db64a8fff0e572552ed56a51763
Yogesh-JI <ygeshverma02349@gmail.com>
2023-04-11 22:15:11 +0530

    just it

--- unary operator.c
+++ unary operator.c
@@ -2,31 +2,31 @@
 #include <conio.h>
 
 int main()
-//{
-//
-//int x= 3,y,z;
-//y=x++;
-//z=++x;
-//printf("\n%d,%d,%d",x,y,z);
-//
-//}
+{
+
+int x= 3,y,z;
+y=x++;
+z=++x;
+printf("\n%d,%d,%d",x,y,z);
+
+}
 
 
-//{
-//	int x,y,z;
-//	printf("enter x= ");
-//	scanf("%d",&x);
-//	y=x++;
-//    z=++x;
-//    printf("\n%d,%d,%d",x,y,z);
-//}
+{
+	int x,y,z;
+	printf("enter x= ");
+	scanf("%d",&x);
+	y=x++;
+   z=++x;
+   printf("\n%d,%d,%d",x,y,z);
+}
 
-//{
-//	int a=1,b=1,c;
-//	c=a++ +b;
-//	printf("\n%d,%d,%d",a,b,c);
-//	
-//}
+{
+	int a=1,b=1,c;
+	c=a++ +b;
+	printf("\n%d,%d,%d",a,b,c);
+	
+}
 
 {
 	int x,y,z;
