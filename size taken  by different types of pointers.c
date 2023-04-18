5a064ad44547fe4fe419816623a4077af181a6ef
Yogesh-JI <yogeshverma02349@gmail.com>
2023-04-18 22:01:44 +0530

    just it

---
+++ size taken  by different types of pointers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int *pnum;
+    char *pch;
+    float *pfnum;
+    double *pdnum;
+    long *plnum;
+    printf("\nsize of integer pointer=%d",sizeof(pnum));
+    printf("\nsize of integer pointer=%d",sizeof(pch));
+    printf("\nsize of integer pointer=%d",sizeof(pfnum));
+    printf("\nsize of integer pointer=%d",sizeof(pdnum));
+    printf("\nsize of integer pointer=%d",sizeof(plnum));
+
+}
