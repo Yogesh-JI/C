a94e2283349e2e20d69a28ea71b85e24fe2c10fe
Yogesh-JI <yogeshverma02349@gmail.com>
2023-04-24 22:58:07 +0530

    types of pointer

---
+++ pointer arithemetic.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int arr[]=[1,2,3,4,5,6,7,8,9];
+    int *p1,*p2;
+    p1=arr;
+    p1++;
+    // 
+    printf("\n%d",*p1);
+    p1--;
+    // 
+    printf("\n%d",*p1);
+    p1=p1+2;
+    printf("\n%d",*p1);
+    p1=p1-2;
+    p1=p1+2;
+    p2=&arr[4];
+    printf("\n%d",p2,p1);
+
+    while(p1<=p2)
+    {
+        printf("\n%d",*p1);
+        p1++;
+    }
+
+    // the following oprations are invalid
+    // p1+p2;
+    // p1/p2;
+    // p1*p2;
+    // p1%p1;
+    // p1*2;
+    // p1/2;
+    // p1%2;
+}
