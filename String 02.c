b5cf1d53753c2813b918c7851eec45c96ab81dd1
Yogesh-JI <yogeshverma02349@gmail.com>
2023-04-28 21:30:45 +0530

    String

---
+++ String 02.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include <string.h>int main()
+{
+    char ori[20],dup[20];
+    char*2;
+    printf("\nenter your name:");
+    gets(ori);
+    z=strcpy(dup,ori);
+    printf("\nOriginal string is:%s",ori);
+    printf("\nDuplicate string is:%s",dup);
+    printf("\nThe value of z is:%s",z);
+}
