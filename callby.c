122e40fc8ba1408c5c91ab84d9dc00c0084620df
Yogesh-JI <yogeshverma02349@gmail.com>
2023-05-21 13:34:33 +0530

    it is

--- callby.c
+++ callby.c
@@ -1,14 +1,47 @@
+// --------------call by reference
 #include<stdio.h>
 int cubebyvalue (int n);
-int main(void)
+int main()
 {
     int number=5;
-    printf("the riginal value of the number is %d",number);
-    cubebyvalue (number);
+    // int *ptr=&number;
+    printf("the original value of the number is %d",number);
+    cube (&number);
     printf("\nthe new value of number is %d\n",number);
 }
-int cubebyvalue (int n)
+int cube (int *n)
 {
-     return n*n*n;
+     return *n+=1;
     
 }
+
+// void swap(int x, int y)
+// {
+// int temp;
+// temp=x;
+// x=y;
+// y=temp;
+// }
+// void main()
+// { 
+// int r=10, v=20; 
+// swap(r, v);  // passing value to function
+// printf("\nValue of r: %d",r);
+// printf("\nValue of v: %d",v);
+// } 
+
+
+// void swap(int *x, int *y)
+// {
+// int temp;
+//  temp=*x;
+// *x=*y;
+// *y=temp;
+// }
+// void main()
+// { 
+// int r=10, v=20; 
+// swap(&r, &v);  // passing value to function
+// printf("\nValue of r: %d",r);
+// printf("\nValue of v: %d",v);
+// }
