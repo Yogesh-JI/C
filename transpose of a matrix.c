bcde65b8ed5158dda026377999259b8978cab6fb
Yogesh-JI <yogeshverma02349@gmail.com>
2023-05-25 16:09:00 +0530

    it is

--- transpose of a matrix.c
+++ transpose of a matrix.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
 	float a[3][3],b[3][3],c[3][3];
@@ -8,23 +9,23 @@ int main()
 	{
 		for(j=0;j<3;j++)
 		{
-			printf("Enter the %a%d%d",i,j);
+			printf("Enter the %d%d: ",i,j);
 			scanf("%i",&a[i][j]);
 		}
 	}
-}
 
-	printf("Enter the elements of 1ST matrix\n");
+
+	printf("Enter the elements of 2nd matrix\n");
 	for(i=0;i<3;i++)
 	{
 		for(j=0;j<3;j++)
 		{
-			printf("Enter the %a%d%d",i,j);
+			printf("Enter the %d%d: ",i,j);
 			scanf("%i",&b[i][j]);
 		}
 	}
 	//adding the matrix
-	for(i=o;i<3;i++)
+	for(i=0;i<3;i++)
 	{
 		for(j=0;j<3;j++);
 		{
@@ -32,12 +33,13 @@ int main()
 		}
 	}
 	//display the sum
-	for(i=0;i<3;i++)
-	{
-		for(j=0;j<3;j++)
-		{
-			printf("%1f\t",c[i][j]);
-		}
-		pritnf("\n");
-	}
+	// for(i=0;i<3;i++)
+	// {
+	// 	for(j=0;j<3;j++)
+	// 	{
+	// 		printf("%f\t",c[i][j]);
+	// 	}
+	// 	 pritnf("\n",);
+	// }
 	return 0;
+}
