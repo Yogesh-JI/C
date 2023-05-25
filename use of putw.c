799b6747e37a9eb39dea6dfed704541b51efc424
Yogesh-JI <yogeshverma02349@gmail.com>
2023-05-25 16:10:15 +0530

    it is

---
+++ use of putw.c
@@ -0,0 +1,11 @@
+#include<conio.h>
+void main(){
+    file *fptr;
+    int i,n;
+    fptr=fopen("b.txt","w");
+    for (i=1;i<=10;i++)
+    {
+        printf("enter number");
+        scanf("%d"&n);
+    }
+}
