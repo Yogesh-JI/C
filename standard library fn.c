238a1e8f6503e20368821002f4e998ffe0cc8d0a
Yogesh-JI <yogeshverma02349@gmail.com>
2023-05-26 21:25:33 +0530

    just it

---
+++ standard library fn.c
@@ -0,0 +1,52 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main()
+// {
+//     double d;
+//     d=atof("99.2");
+//     printf("%.2lf",d);
+//     return 0;
+// }
+
+
+
+// {
+//     char x[100];
+//     printf("Enter the digit string:");
+//     gets(x);
+//     int i;
+//     i=atoi(x);
+//     printf("%d",i);
+// }
+
+
+// {
+//     long int i;
+//     char x[]="100000000";
+//     i=atol(x);
+//     printf("%ld",i);
+// }
+
+
+// {
+// int a=123;
+// char str[100];
+// itoa(a,str,2);
+// printf("the binary value:%s\n",str);
+// itoa(a,str,10);
+// printf("The decimal value:%s\n",str);
+// itoa(a,str,16);
+// printf("The hexadecimal value:%s\n",str);
+// itoa(a,str,8);
+// printf("The octal value:%s\n",str);
+// }
+
+
+{
+char x='a'+ 1;
+printf("%c",x);
+printf("\n%c",++x);
+printf("\n%c",--x);
+printf("\n%c",--x);
+
+}
